f219ce2de8d1ea850d3c3fdac21ec539fa3b44f4
alberti-tu <albertalvarezmiron@gmail.com>
2021-03-30 13:38:32 +0200

    shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared.module';
 
 @NgModule({
 	declarations: [ AppComponent ],
-	imports: [ BrowserModule ],
+	imports: [
+		BrowserModule,
+		SharedModule
+	],
 	providers: [],
 	bootstrap: [ AppComponent ]
 })
